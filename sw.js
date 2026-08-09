78f43072b0081145be7ff252d5e4a4d0976ceb2e
gabifreitassilva-pixel <gabifreitassilva@gmail.com>
2026-08-09 20:41:12 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,15 @@
-const CACHE_NAME = 'manager-pro-cache-v1';
-const urlsToCache = [
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'shopee-converter-v1';
 
-// Instala o motor no celular e salva os arquivos iniciais
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (e) => {
+    self.skipWaiting();
 });
 
-// Responde rápido usando os arquivos salvos se o celular estiver sem rede
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response; // Retorna o cache se encontrar
-        }
-        return fetch(event.request); // Se não, busca na internet
-      })
-  );
+self.addEventListener('activate', (e) => {
+    self.clients.claim();
 });
 
-// Limpa caches antigos quando houver atualização
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
 });
